93958c71f224d93dfbee8043ab70a7b74f58088f
dipendra <dipendra@iitk.ac.in>
2016-09-30 16:29:39 +0530

    removing binaries

--- build/include/mutual_information.hpp
+++
@@ -1,4 +0,0 @@
-#include <mireg.hpp>
-
-ld mutual_information(std::pair<pii, vvd>& feature_map1,std::pair<pii, vvd>& feature_map2, int hist_size);
-
--- build/src/helper/CMakeFiles/project_classes.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../../lib/libproject_classes.a"
-)
