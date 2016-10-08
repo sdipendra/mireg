6126c9dd7740dcfeb715f5b551064a5846724546
dipendra <dipendra@iitk.ac.in>
2016-10-08 11:13:35 +0530

    fixed some mistakes

--- build/include/registration.hpp
+++
@@ -1,4 +0,0 @@
-#include <mireg.hpp>
-
-bool multires_registration(std::vector<point>& reading, std::vector<point>& reference, Eigen::Matrix4d& transformation_mat, double min_cell_size, int hist_size, std::string& map);
-
--- build/src/helper/CMakeFiles/pcl_helper.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../../lib/libpcl_helper.a"
-)
